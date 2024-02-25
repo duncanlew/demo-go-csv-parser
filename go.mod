554f2d74e26f6144b428907b2a789009fc715bff
Duncan <post@duncanlew.nl>
2024-02-25 21:55:48 +0100

    initial stuff

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module demo-go-csv-parser
 
 go 1.21.6
+
+require github.com/gocarina/gocsv v0.0.0-20231116093920-b87c2d0e983a // indirect
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"log"
+	"os"
+)
+
+func main() {
+	csvFile, err := os.OpenFile("example.csv", os.O_RDWR|os.O_CREATE, os.ModePerm)
+	if err != nil {
+		log.Fatalf("Failed to open CSV file: %v", err)
+	}
+	defer csvFile.Close()
+}
