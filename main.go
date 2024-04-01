1cdc471cbb57d4372a85090debed88e2ecf24af3
Duncan Lew <duncanlew@users.noreply.github.com>
2024-04-01 22:15:54 +0200

    Add comments (#3)

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"github.com/gocarina/gocsv"
 	"os"
+
+	"github.com/gocarina/gocsv"
 )
 
 type Article struct {
@@ -22,13 +23,17 @@ func main() {
 }
 
 func ReadCsv() []*Article {
+	// Try to open the example.csv file in read-write mode.
 	csvFile, csvFileError := os.OpenFile("example.csv", os.O_RDWR, os.ModePerm)
+	// If an error occurs during os.OpenFIle, panic and halt execution.
 	if csvFileError != nil {
 		panic(csvFileError)
 	}
+	// Ensure the file is closed once the function returns
 	defer csvFile.Close()
 
 	var articles []*Article
+	// Parse the CSV data into the articles slice. If an error occurs, panic.
 	if unmarshalError := gocsv.UnmarshalFile(csvFile, &articles); unmarshalError != nil {
 		panic(unmarshalError)
 	}
@@ -36,24 +41,34 @@ func ReadCsv() []*Article {
 	return articles
 }
 
+func GetInboxArticles(articles []*Article) []*Article {
+	// Initialize an empty slice to store inbox articles
+	var inboxArticles []*Article
+
+	// Iterate through each article in the provided slice.
+	for _, article := range articles {
+		// Check if the article's Location is equal to inbox
+		if article.Location == "inbox" {
+			// If the article's location is inbox, add it to the inboxArticles slice
+			inboxArticles = append(inboxArticles, article)
+		}
+	}
+
+	return inboxArticles
+}
+
 func WriteCsv(articles []*Article) {
+	// Open result.csv for writing; create it if it doesn't exist, or overwrite it if it already exists.
 	resultFile, resultFileError := os.OpenFile("result.csv", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, os.ModePerm)
+
+	// Check for errors when opening or creating the file. If there's an error, panic.
 	if resultFileError != nil {
 		panic(resultFileError)
 	}
 	defer resultFile.Close()
 
+	// Marshal the articles into the CSV format and write them to the result.csv file
 	if marshalFileError := gocsv.MarshalFile(&articles, resultFile); marshalFileError != nil {
 		panic(marshalFileError)
 	}
-}
-
-func GetInboxArticles(articles []*Article) []*Article {
-	var filteredArticles []*Article
-	for _, article := range articles {
-		if article.Location == "inbox" {
-			filteredArticles = append(filteredArticles, article)
-		}
-	}
-	return filteredArticles
 }
